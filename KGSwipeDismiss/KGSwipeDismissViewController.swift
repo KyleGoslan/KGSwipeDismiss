caa38d2cd3a2a7bcfb0fde863a09bed7be4c0456
KyleGoslan <kylegoslan@me.com>
2016-11-22 18:28:59 +0000

    Opened class

--- KGSwipeDismiss/KGSwipeDismissViewController.swift
+++ KGSwipeDismiss/KGSwipeDismissViewController.swift
@@ -4,22 +4,23 @@ import UIKit
 //  @objc optional func willDismiss()
 //}
 
-class SwipeDismissViewController: UIViewController {
+open class SwipeDismissViewController: UIViewController {
   
-  @IBOutlet var swipeView: UIView!
+  @IBOutlet public var swipeView: UIView!
   
-  var swipeAmount: CGFloat = 300
+  public var swipeAmount: CGFloat = 300
   
-  private var startLocation: CGPoint!
+  public var startLocation: CGPoint!
   
 //  weak var delegate: SwipeDismissViewControllerDelegate?
   
-  override func viewDidLoad() {
+  override open func viewDidLoad() {
     view.backgroundColor = .black
     view.backgroundColor = view.backgroundColor?.withAlphaComponent(0.8)
     view.addSubview(swipeView)
-    
-    startLocation = swipeView.center
+  
+    print(startLocation)
+    print(swipeView.center)
     super.viewDidLoad()
     
     let pan = UIPanGestureRecognizer(target: self, action: #selector(swipe(pan:)))
@@ -54,7 +55,7 @@ class SwipeDismissViewController: UIViewController {
   
   func reset() {
     UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: { [unowned self] in
-      self.swipeView.center = self.startLocation
+      self.swipeView.center.y = self.startLocation.y
       self.swipeView.alpha = 1
       self.view.backgroundColor = self.view.backgroundColor?.withAlphaComponent(0.8)
     }, completion: nil)
